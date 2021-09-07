6a677595384f756853b4f6c9758353c99432d30d
Jared Boleyn <jsboleyn@gmail.com>
2021-09-06 22:31:38 -0500

    fixed some issurs
    todo: add recommended game, and add reschedule game

--- src/app/add-game/add-game.component.ts
+++ src/app/add-game/add-game.component.ts
@@ -3,12 +3,14 @@ import { ScheduleService } from '../schedule.service';
 import { School } from '../school';
 import { AddGameRequest } from '../addGameRequest';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { SchoolScheduleComponent } from '../school-schedule/school-schedule.component';
 
 @Component({
   selector: 'app-add-game',
   templateUrl: './add-game.component.html',
   styleUrls: ['./add-game.component.css']
 })
+
 export class AddGameComponent implements OnInit {
 
   // school!: School;
@@ -21,26 +23,22 @@ export class AddGameComponent implements OnInit {
   isHomeTeam?: boolean;
   //addGameRequest!: AddGameRequest;
 
-  constructor(private scheduleService: ScheduleService, private route: ActivatedRoute) { }
+
+
+  constructor(private scheduleService: ScheduleService, private route: ActivatedRoute, private schoolScheduleComponent: SchoolScheduleComponent) { }
 
   ngOnInit(): void {
-    this.tgid = parseInt(this.route.snapshot.paramMap.get('tgid')!, 10);
-    this.scheduleService.getEmptyWeeks(this.tgid).subscribe((data: number[]) => {
-      console.log(data);
-      this.availableWeeks = data;
-    })
-
+    this.route.params.subscribe(params => {
+      this.tgid = parseInt(this.route.snapshot.paramMap.get('tgid')!, 10);
+      this.scheduleService.getEmptyWeeks(this.tgid).subscribe((data: number[]) => {
+        console.log(data);
+        this.availableWeeks = data;
+      })
+    });
   }
 
   async add() {
-    //this.schedule = this.schedule.filter(g => g !== game);
-    //addGameRequest : new AddGameRequest;
-    //category: Category = new Category();
-    //const addGameRequest: AddGameRequest = new AddGameRequest();
-
-    
-
-    if(this.isHomeTeam){
+    if (this.isHomeTeam) {
       const addGameRequest: AddGameRequest = {
         homeId: this.tgid,
         awayId: this.selectedOpponent.tgid,
@@ -48,7 +46,7 @@ export class AddGameComponent implements OnInit {
       };
       await this.scheduleService.addGame(addGameRequest);
     }
-    else if(!this.isHomeTeam) {
+    else if (!this.isHomeTeam) {
       const addGameRequest: AddGameRequest = {
         awayId: this.tgid,
         homeId: this.selectedOpponent.tgid,
@@ -56,10 +54,8 @@ export class AddGameComponent implements OnInit {
       };
       await this.scheduleService.addGame(addGameRequest);
     }
-    
-    // await sleep(1000);
-    //this.setData();
-    //this.dataSource.setData(this.displayedColumns);
+
+    this.schoolScheduleComponent.setData()
   }
 
   changeWeek(): void {
--- src/app/school-schedule/school-schedule.component.ts
+++ src/app/school-schedule/school-schedule.component.ts
@@ -5,7 +5,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { School } from '../school';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { AddGameRequest } from '../addGameRequest';
 
 
 @Component({
@@ -20,6 +21,9 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
     ]),
   ],
 })
+// @Injectable({
+//   providedIn: 'root' // just before your class
+// })
 export class SchoolScheduleComponent implements OnInit {
   school!: School;
   clickedRow?: Game;
@@ -27,7 +31,13 @@ export class SchoolScheduleComponent implements OnInit {
   @ViewChild(MatSort, { static: false }) sort!: MatSort;
   schedule: Game[] = [];
   activatedRoute = new ActivatedRoute;
-  tgid?: number;
+  tgid!: number;
+
+  selectedOpponent!: School;
+  selectedWeek!: number;
+  availableOpponents: School[] = [];
+  availableWeeks: number[] = [];
+  isHomeTeam!: boolean;
 
   displayedColumns: string[] = ['game', 'week', 'opponent', 'conferenceGame'];
   dataSource = new MatTableDataSource(this.schedule);
@@ -35,17 +45,50 @@ export class SchoolScheduleComponent implements OnInit {
 
   constructor(private scheduleService: ScheduleService, private route: ActivatedRoute) { }
 
+  async add() {
+
+    if (this.isHomeTeam === true) {
+      const addGameRequest: AddGameRequest = {
+        homeId: this.tgid,
+        awayId: this.selectedOpponent.tgid,
+        week: this.selectedWeek
+      };
+      await this.scheduleService.addGame(addGameRequest);
+    }
+    else if (this.isHomeTeam === false) {
+      const addGameRequest: AddGameRequest = {
+        homeId: this.selectedOpponent.tgid,
+        awayId: this.tgid,
+        week: this.selectedWeek
+      };
+      await this.scheduleService.addGame(addGameRequest);
+    }
+    this.setData()
+    this.setAddGameData();
+  }
+
   async delete(game: Game) {
     this.schedule = this.schedule.filter(g => g !== game);
     await this.scheduleService.deleteGame(this.school.tgid, game.week);
     // await sleep(1000);
     this.setData();
+    this.setAddGameData();
     //this.dataSource.setData(this.displayedColumns);
+  }
+
+  changeWeek(): void {
+    //this.selectedOpponent = undefined;
+    this.scheduleService.getAvailableOpponents(this.tgid, this.selectedWeek).subscribe((data: School[]) => {
+      console.log(data);
+      this.availableOpponents = data;
+    })
   }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
+      
       this.setData();
+      this.setAddGameData();
     });
     //this.setData();
   }
@@ -62,6 +105,14 @@ export class SchoolScheduleComponent implements OnInit {
       this.dataSource = new MatTableDataSource(this.schedule);
       this.dataSource.sort = this.sort;
     })
+  }
+
+  setAddGameData(): void {
+    this.tgid = parseInt(this.route.snapshot.paramMap.get('tgid')!, 10);
+      this.scheduleService.getEmptyWeeks(this.tgid).subscribe((data: number[]) => {
+        console.log(data);
+        this.availableWeeks = data;
+      })
   }
 
   
