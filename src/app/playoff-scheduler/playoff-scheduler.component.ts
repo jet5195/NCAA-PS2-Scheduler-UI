3c62c99d8892372167bf22cabd919b2e2265f23c
jet5195 <jsboleyn@gmail.com>
2022-07-06 16:52:48 -0500

    minor changes

--- src/app/playoff-scheduler/playoff-scheduler.component.ts
+++ src/app/playoff-scheduler/playoff-scheduler.component.ts
@@ -14,6 +14,60 @@ export class PlayoffSchedulerComponent implements OnInit {
   playoffSchools: School[] = [];
   teamCount: number = 0;
 
+  //Week 16 is conference title games
+  //Week 17 - 20 are bowl games
+  // 16 team playoffs 
+  /*
+  8 games week 17
+  4 games week 18
+  2 games week 19
+  1 games week 20
+  */
+
+  // 8 team playoffs 
+  /*
+  4 games week 18
+  2 games week 19
+  1 games week 20
+  */
+
+  // 4 team playoffs 
+  /*
+  2 games week 19
+  1 games week 20
+  */
+
+  //pseudo code
+  /*
+  1. WVU
+  4. Oklahoma
+
+  2. Kentucky
+  3. USC
+
+  original bowls
+  rose
+  USC vs OSU
+
+  orange (natty)
+  WVU vs Kentucky
+
+  fiesta
+  Oklahoma vs VT
+
+  sugar
+  Tennessee vs FSU
+
+  new bowls
+  rose
+  USC vs Kentucky
+
+  orange
+  ?? vs ??
+
+
+  */
+
   constructor(private dataService: DataService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
